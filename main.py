239e45ba49a53f34632ec1c3a9b55e2c3b728541
Niko <zoikasz.nikolasz@gmail.com>
2023-08-27 17:32:16 +0200

    Final product

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import pandas as pd
+
+
+app = Flask("Website")
+df = pd.read_csv("data_small/stations.txt", skiprows=17)
+df = df[["STAID", "STANAME                                 "]]
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=df.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def weather_data(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
+    return {"temperature": temperature, "station": station, "date": date}
+
+@app.route("/api/v1/<station>")
+def all_temperatures(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20,)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+app.run(debug=True, port=5000)
